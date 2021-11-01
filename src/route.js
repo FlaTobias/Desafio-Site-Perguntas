3d5f5f41fb7be15478e50a05c7abaf5dcb515ec5
Flavio Tobias <flavioa.tobias@gmail.com>
2021-11-01 09:43:14 -0300

    Finalizado

--- src/route.js
+++ src/route.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const QuestionController = require('./controllers/QuestionController')
+const RoomController = require('./controllers/RoomController')
 
 const route = express.Router()
 
-route.get('/', (req, res) => res.render("index"))
-route.get('/room', (req,res) => res.render("room"))
-route.get('/create-pass', (req,res) => res.render("create-pass"))
+route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))
+route.get('/room/:room', RoomController.open)
+route.get('/create-pass', (req,res) => res.render("index", {page: 'create-pass'}))
+route.post('/question/:room/:question/:action', QuestionController.index)
+route.post('/create-room', RoomController.create)
+route.post('/question/create/:room', QuestionController.create)
+route.post('/enterroom', RoomController.enter)
 
 module.exports = route
